7fd5be125f38c510cfab173e03b495fa1f48b741
Gabriel Belther <belthergabriel@gmail.com>
2023-12-17 18:07:14 -0300

    feat: create dropdown component

---
+++ src/components/globals/Dropdown/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import * as ReactDropdown from '@radix-ui/react-dropdown-menu'
+import { cn } from '@/utils/cn'
+
+const Content = React.forwardRef<
+  React.ElementRef<typeof ReactDropdown.Content>,
+  React.ComponentPropsWithoutRef<typeof ReactDropdown.Content>
+>(({ className, ...rest }, ref) => (
+  <ReactDropdown.Content
+    {...rest}
+    className={cn('rounded border border-gray-300 bg-white p-1', className)}
+    ref={ref}
+  />
+))
+
+const Dropdown = {
+  ...ReactDropdown,
+  Content: Content,
+}
+
+export default Dropdown
