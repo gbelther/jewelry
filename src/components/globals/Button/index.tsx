b91d665975448d982a99ed6efa5cb707eedd41ee
Gabriel Belther <belthergabriel@gmail.com>
2023-12-17 18:07:07 -0300

    feat: create button component

---
+++ src/components/globals/Button/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { cn } from '@/utils/cn'
+
+const Button = React.forwardRef<
+  React.ElementRef<'button'>,
+  React.ComponentPropsWithoutRef<'button'>
+>(({ className, ...rest }, ref) => {
+  return (
+    <button
+      {...rest}
+      className={cn(
+        'rounded-lg border border-primary-700 bg-primary-700 px-2 py-1 font-medium text-white transition-all hover:brightness-90 active:brightness-75',
+        className,
+      )}
+      ref={ref}
+    />
+  )
+})
+
+export default Button
